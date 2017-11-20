9f75c9e4b73f5850e185600a5af05eee823ac7a5
Damian Jóźwiak <jozwiak.damian02@gmail.com>
2017-11-20 13:56:43 +0100

    Add files via upload

---
+++ labSpirala.m
@@ -0,0 +1,36 @@
+x = 0;
+y = 1;
+z = 0;
+
+x0 = 0;
+y0 = 1;
+z0 = 0;
+
+s = serial('/dev/ttyUSB2');
+set(s,'BaudRate',57600);
+fopen(s);
+
+for t=0:0.1:10
+    x = sin(t);
+    y = cos(t);
+    z = t;
+    
+    
+    
+    a=ceil([x-x0 y-y0 z-z0]*1000)   
+    str = sprintf('x %d y %d z %d\n',a(1),a(2),a(3))
+    fprintf(s, str);
+    plot3(x,y,z,'o');
+    hold on;
+    pause(0.3);
+    
+    x0 = x;
+    y0 = y;
+    z0 = z;
+    
+ 
+end
+
+fclose(s);
+delete(s);
+clear s;
